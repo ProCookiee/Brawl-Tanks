88261fb24bb540307650a21d0b75f6fd9005f77b
tadejrebernjak <tadej.rebernjak2@gmail.com>
2024-04-21 14:50:25 +0200

    Main Menu

--- Brawl-Tanks/Assets/GameManager.cs
+++ Brawl-Tanks/Assets/GameManager.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI goalText;
+    public TextMeshProUGUI gamemodeText;
+
     public GameObject player1;
     public GameObject player2;
 
     void Start()
     {
+        goalText.text = "First to: " + GameState.goal.ToString();
+        gamemodeText.text = GameState.gamemode;
+
         Instantiate(player1, new Vector3(-4, 0, 0), Quaternion.identity);
         Instantiate(player2, new Vector3(4, 0, 0), Quaternion.identity);
 
