4447d4fb95cf39fd0d7dfbf7fa5c80f93557614c
tadejrebernjak <tadej.rebernjak2@gmail.com>
2024-05-10 18:51:46 +0200

    Moved map generation logic to a separate script

--- Brawl-Tanks/Assets/GameManager.cs
+++ Brawl-Tanks/Assets/GameManager.cs
@@ -4,36 +4,6 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public enum NodeState
-{
-    Available,
-    Current,
-    Completed
-}
-
-public class MapCell
-{
-    public NodeState state;
-    public int y;
-    public int x;
-    public bool top;
-    public bool right;
-    public bool bottom;
-    public bool left;
-
-    // Constructor to initialize a map cell with all walls initially present or absent
-    public MapCell(int y, int x,  bool top, bool right, bool bottom, bool left)
-    {
-        this.y = y;
-        this.x = x;
-        this.state = NodeState.Available;
-        this.top = top;
-        this.right = right;
-        this.bottom = bottom;
-        this.left = left;
-    }
-}
-
 public class GameManager : MonoBehaviour
 {
     // Singleton instance of the GameManager
@@ -75,11 +45,12 @@ public class GameManager : MonoBehaviour
         Instantiate(player1, GenerateSpawnLocation(1), Quaternion.identity);
         Instantiate(player2, GenerateSpawnLocation(2), Quaternion.identity);
 
-        //Nastavim zacetni rezultat
+        // Nastavim zacetni rezultat
         P1ScoreText.text = "P1: " + GameState.P1Score;
         P2ScoreText.text = "P2: " + GameState.P2Score;
 
-        GenerateMap();
+        // Zgeneriram mapo
+        MapGenerator.GenerateMap(wallPrefab);
     }
 
     // Update is called once per frame
@@ -111,185 +82,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    void GenerateMap()
-    {
-        int rows = 4;
-        int cols = 8;
-
-        MapCell[,] map = new MapCell[rows, cols];
-
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < cols; col++)
-                map[row, col] = new MapCell(row, col, true, true, true, true);
-        }
-
-        // DFS
-        List<MapCell> currentPath = new List<MapCell>();
-        List<MapCell> completedNodes = new List<MapCell>();
-
-        currentPath.Add(map[Random.Range(0, rows), Random.Range(0, cols)]);
-        currentPath[0].state = NodeState.Current;
-        
-        while (completedNodes.Count < rows*cols)
-        {
-            List<MapCell> possibleNodes = new List<MapCell>(); 
-            List<string> possibleDirections = new List<string>();
-
-            MapCell currentNode = currentPath[currentPath.Count - 1];
-
-            if (currentNode.y > 0)
-            {
-                MapCell neighbour = map[currentNode.y - 1, currentNode.x];
-                if (!completedNodes.Contains(neighbour) && !currentPath.Contains(neighbour)) {
-                    possibleDirections.Add("up");
-                    possibleNodes.Add(neighbour);
-                }
-            }
-            if (currentNode.y < rows - 1)
-            {
-                MapCell neighbour = map[currentNode.y + 1, currentNode.x];
-                if (!completedNodes.Contains(neighbour) && !currentPath.Contains(neighbour)) {
-                    possibleDirections.Add("down");
-                    possibleNodes.Add(neighbour);
-                }
-            }
-            if (currentNode.x > 0)
-            {
-                MapCell neighbour = map[currentNode.y, currentNode.x - 1];
-                if (!completedNodes.Contains(neighbour) && !currentPath.Contains(neighbour)) {
-                    possibleDirections.Add("left");
-                    possibleNodes.Add(neighbour);
-                }
-            }
-            if (currentNode.x < cols - 1)
-            {
-                MapCell neighbour = map[currentNode.y, currentNode.x + 1];
-                if (!completedNodes.Contains(neighbour) && !currentPath.Contains(neighbour)) {
-                    possibleDirections.Add("right");
-                    possibleNodes.Add(neighbour);
-                }
-            }
-
-            if (possibleDirections.Count > 0)
-            {
-                int direction = Random.Range(0, possibleDirections.Count);
-                MapCell chosenNode = possibleNodes[direction];
-
-                switch (possibleDirections[direction])
-                {
-                    case "up":
-                        currentNode.top = false;
-                        chosenNode.bottom = false;
-                        break;
-                    case "down":
-                        currentNode.bottom = false;
-                        chosenNode.top = false;
-                        break;
-                    case "left":
-                        currentNode.left = false;
-                        chosenNode.right = false;
-                        break;
-                    case "right":
-                        currentNode.right = false;
-                        chosenNode.left = false;
-                        break;
-                    default:
-                        break;
-                }
-
-                currentPath.Add(chosenNode);
-                chosenNode.state = NodeState.Current;
-            } 
-            else
-            {
-                completedNodes.Add(currentNode);
-                currentNode.state = NodeState.Completed;
-                currentPath.RemoveAt(currentPath.Count - 1);
-            }
-        }
-
-        // Remove some more walls randomly
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < cols; col++)
-            {
-                if (row > 0 && map[row, col].top && Random.Range(0, 10) == 0)
-                {
-                    map[row, col].top = false;
-                    map[row - 1, col].bottom = false;
-                }
-                if (row < rows - 1 && map[row, col].bottom && Random.Range(0, 10) == 0)
-                {
-                    map[row, col].bottom = false;
-                    map[row + 1, col].top = false;
-                }
-                if (col > 0 && map[row, col].left && Random.Range(0, 10) == 0)
-                {
-                    map[row, col].left = false;
-                    map[row, col - 1].right = false;
-                }
-                if (col < cols - 1 && map[row, col].right && Random.Range(0, 10) == 0)
-                {
-                    map[row, col].right = false;
-                    map[row, col + 1].left = false;
-                }
-            }
-        }
-
-        // Generate wall objects
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < cols; col++)
-            {
-                if (col < cols - 1 && map[row, col].right)
-                {
-                    GameObject wall = Instantiate(wallPrefab, new Vector3(ConvertX(col+1), ConvertY(row) - 1f, 0f), Quaternion.identity);
-                    wall.transform.localScale = new Vector3(0.1f, 2.1f, 0.2f);
-                }
-                if (row < rows - 1 && map[row, col].bottom)
-                {
-                    GameObject wall = Instantiate(wallPrefab, new Vector3(ConvertX(col) + 1f, ConvertY(row+1), 0f), Quaternion.identity);
-                    wall.transform.localScale = new Vector3(2.1f, 0.1f, 0.2f);
-                }
-            }
-        }
-    }
-
     Vector3 GenerateSpawnLocation(int player)
     {
         float x, y;
 
+        // mapa je 8x4, torej ConvertY prejme 0-4, ConvertX pa 0-8
         if (player == 1)
-            x = ConvertX(Random.Range(0, 3));
+            x = MapGenerator.ConvertX(Random.Range(0, 3));
         else
-            x = ConvertX(Random.Range(5, 8));
+            x = MapGenerator.ConvertX(Random.Range(5, 8));
 
-        y = ConvertY(Random.Range(0, 4));
+        y = MapGenerator.ConvertY(Random.Range(0, 4));
 
+        // x+1f in y-1f, da premaknem iz zgornje levega kota celice v sredino celice
         return new Vector3(x+1f, y-1f, 0f);
-    }
-
-    float ConvertX(float x)
-    {
-        return x*2 - 8f;
-    }
-
-    float ConvertY(float input)
-    {
-        // Define the range and corresponding values
-        float minInput = 0f;
-        float maxInput = 4f;
-        float minValue = 3.5f;
-        float maxValue = -4.5f;
-
-        // Clamp the input to the range
-        input = Mathf.Clamp(input, minInput, maxInput);
-
-        // Calculate the interpolation factor
-        float t = (input - minInput) / (maxInput - minInput);
-
-        // Perform linear interpolation between minValue and maxValue
-        return Mathf.Lerp(minValue, maxValue, t);
     }
 }
