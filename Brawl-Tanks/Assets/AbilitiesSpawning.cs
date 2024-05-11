dd095337f532c780d18281f196f827418823ab34
zalozniktine59 <tine.zaloznik@student.um.si>
2024-05-11 15:24:16 +0200

    abilites

--- Brawl-Tanks/Assets/AbilitiesSpawning.cs
+++ Brawl-Tanks/Assets/AbilitiesSpawning.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class AbilitiesSpawning : MonoBehaviour
 {
-    private struct AbilityData 
+    public struct SpawnedAbility 
     {
         public GameObject abilityObject;
         public Vector3 location;
@@ -19,12 +19,14 @@ public class AbilitiesSpawning : MonoBehaviour
     //public GameObject wallPrefab;
     //public GameObject player1;
     //public GameObject player2;
+    
 
     public float prefabSize = 0.08f;
     public float minSpawnRate = 3.0f;
     public float maxSpawnRate = 15.0f;
     public int maxAbilities = 6;
     public List<GameObject> abilityPrefabs;
+    private List<SpawnedAbility> spawnedAbilities;
 
     private float nextSpawnTime;
     private List<Vector3> spawnLocations; // Add missing variable declaration
@@ -53,6 +55,8 @@ public class AbilitiesSpawning : MonoBehaviour
     {
         Vector3 spawnLocation = GameManager.instance.GenerateSpawnLocation(3);
         GameObject abilityPrefab = abilityPrefabs[Random.Range(0, abilityPrefabs.Count)];
+
+        //spawnedAbilities.Add(new SpawnedAbility { abilityObject = abilityPrefab, location = spawnLocation });
         GameObject abilityInstance = Instantiate(abilityPrefab, spawnLocation, Quaternion.identity);
         abilityInstance.transform.localScale = new Vector3(prefabSize, prefabSize, 1); // replace with your desired size
 
