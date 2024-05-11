b67d7441f2611902b5596683359f7f988997bf05
zalozniktine59 <tine.zaloznik@student.um.si>
2024-05-11 09:39:59 +0200

    Map generation

---
+++ Brawl-Tanks/Assets/AbilitiesSpawning.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitiesSpawning : MonoBehaviour
+{
+
+    public Vector3 topLeftCorner;
+    public Vector3 bottomRightCorner;
+    public GameObject abilityPrefab;
+    public GameObject wallPrefab;
+    public GameObject player1;
+    public GameObject player2;
+
+    public float abilitySpawnRate = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
